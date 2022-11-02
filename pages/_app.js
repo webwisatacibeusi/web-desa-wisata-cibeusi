171776a9ae17e43498d13cec8a0465a54f3d9c5a
bagussr <bagusssyamsu@gmail.com>
2022-11-03 06:56:02 +0700

    initial project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../utils/extendThemes';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
